4aa9d7ddf6b863922457685aaaf8b7d8f584603b
arohan-agate <arogate05@gmail.com>
2024-10-12 13:53:57 -0700

    co

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+db = sqlite3.connect("neighbor.db")
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    print(request.form.get("username"))
+    print(request.form.get("password"))
+    return render_template("home")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
