638fd7d0f543ab81f7c5f9fc285b8f07735e23f4
arohan-agate <arogate05@gmail.com>
2024-10-12 16:03:14 -0700

    added profile page

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
+from werkzeug.utils import secure_filename
+import os
+from flask_migrate import Migrate
 from models import db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SECRET_KEY'] = 'b1b2f5b74c3b5fdbcdde4f218f6d3e2ff3a1fcd0ad63de452391ef8c9bf4a9d1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Set up your SQLite DB
+app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a strong secret key
+app.config['UPLOAD_FOLDER'] = 'static/uploads'  # Folder for storing profile pictures
 db.init_app(app)
 
-# Create the database tables
+migrate = Migrate(app, db)
+
+# Ensure upload folder exists
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+# Initialize the database tables
 with app.app_context():
     db.create_all()
+
+# States list to populate the dropdown in the profile form
+states = [
+    ('AL', 'Alabama'), ('AK', 'Alaska'), ('AZ', 'Arizona'), ('AR', 'Arkansas'),
+    ('CA', 'California'), ('CO', 'Colorado'), ('CT', 'Connecticut'), ('DE', 'Delaware'),
+    ('FL', 'Florida'), ('GA', 'Georgia'), ('HI', 'Hawaii'), ('ID', 'Idaho'),
+    ('IL', 'Illinois'), ('IN', 'Indiana'), ('IA', 'Iowa'), ('KS', 'Kansas'),
+    ('KY', 'Kentucky'), ('LA', 'Louisiana'), ('ME', 'Maine'), ('MD', 'Maryland'),
+    ('MA', 'Massachusetts'), ('MI', 'Michigan'), ('MN', 'Minnesota'), ('MS', 'Mississippi'),
+    ('MO', 'Missouri'), ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'),
+    ('NH', 'New Hampshire'), ('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NY', 'New York'),
+    ('NC', 'North Carolina'), ('ND', 'North Dakota'), ('OH', 'Ohio'), ('OK', 'Oklahoma'),
+    ('OR', 'Oregon'), ('PA', 'Pennsylvania'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'),
+    ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'),
+    ('VT', 'Vermont'), ('VA', 'Virginia'), ('WA', 'Washington'), ('WV', 'West Virginia'),
+    ('WI', 'Wisconsin'), ('WY', 'Wyoming')
+]
 
 # Home route
 @app.route('/')
@@ -65,12 +92,52 @@ def feed():
         flash('You are not logged in!')
         return redirect(url_for('signin'))
 
+# Profile route (view/edit profile)
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if 'username' not in session:
+        flash('You must be logged in to view this page.')
+        return redirect(url_for('signin'))
+
+    user = User.query.filter_by(username=session['username']).first()
+
+    if request.method == 'POST':
+        # Update profile information
+        user.name = request.form.get('name')
+        user.phone = request.form.get('phone')
+        user.email = request.form.get('email')
+        user.bio = request.form.get('bio')
+        user.age = request.form.get('age')
+        user.sports = request.form.get('sports')
+        user.interests = request.form.get('interests')
+        user.location_city = request.form.get('location_city')
+        user.location_state = request.form.get('location_state')
+        user.languages = request.form.get('languages')
+        user.gender = request.form.get('gender')
+        user.pronouns = request.form.get('pronouns')
+        user.school_work = request.form.get('school_work')
+
+        # Handle profile picture upload
+        if 'profile_picture' in request.files:
+            profile_picture = request.files['profile_picture']
+            if profile_picture.filename != '':
+                filename = secure_filename(profile_picture.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                profile_picture.save(filepath)
+                user.profile_picture = filepath
+
+        db.session.commit()
+        flash('Profile updated successfully!')
+        return redirect(url_for('profile'))
+
+    return render_template('profile.html', user=user, states=states)
+
 # Logout route
 @app.route('/logout')
 def logout():
     session.pop('username', None)  # Remove username from session
     flash('You have been logged out.')
-    return render_template('logout.html')  # Render a logout confirmation page
+    return redirect(url_for('signin'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -6,6 +6,20 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    name = db.Column(db.String(150), nullable=True)
+    phone = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(150), nullable=True)
+    bio = db.Column(db.Text, nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    sports = db.Column(db.String(200), nullable=True)
+    interests = db.Column(db.String(200), nullable=True)
+    location_city = db.Column(db.String(100), nullable=True)
+    location_state = db.Column(db.String(50), nullable=True)
+    languages = db.Column(db.String(200), nullable=True)
+    gender = db.Column(db.String(50), nullable=True)
+    pronouns = db.Column(db.String(50), nullable=True)
+    school_work = db.Column(db.String(150), nullable=True)
+    profile_picture = db.Column(db.String(250), nullable=True)  # To store image file path
 
     def __repr__(self):
         return f'<User {self.username}>'
